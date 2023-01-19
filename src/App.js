f1b9a867d9622ea35c6765a068fcb97343998009
Smartech-git <chukwusmartchukwuemeka@gmail.com>
2023-01-19 17:17:39 +0100

    organisation updates and fonts linked

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from './Components/HeaderComponent/Header';
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className = "App">
+      <div className='AppContents'>
+        <Header/>
+        <Routes>
+          <Route path="/"   element={<Home/>}/>
+          <Route path='/About' element={<About/>}/>
+          <Route path= "/Contact" element={<Contact/>}/>
+        </Routes>
+      </div> 
     </div>
+   
   );
 }
 

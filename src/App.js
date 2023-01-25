0d1755b957162ac641f66b9fb6896577a31d3ec4
Smartech-git <chukwusmartchukwuemeka@gmail.com>
2023-01-25 17:02:55 +0100

    initialized footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 import Home from './Pages/Home'
 import About from './Pages/About'
 import Contact from './Pages/Contact'
@@ -17,6 +18,7 @@ function App() {
           <Route path='/About' element={<About/>}/>
           <Route path= "/Contact" element={<Contact/>}/>
         </Routes>
+        <Footer/>
       </div> 
     </div>
    

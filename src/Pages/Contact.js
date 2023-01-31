4b1d3a85e701298069f8767960bd4c2c5ab669a8
Smartech-git <chukwusmartchukwuemeka@gmail.com>
2023-01-31 10:26:27 +0100

    added section1 component

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import ContactSection1 from '../Components/Contact/ContactSection1/ContactSection1'
+import './Contact.css'
 
 export default function Contact() {
 
   return (
-    <div>Contact</div>
+    <div className='ContactPage'>
+        <ContactSection1/>
+    </div>
   )
 }
